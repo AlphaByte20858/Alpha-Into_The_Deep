9a34ca8b857f5770ba660a9b2a924357109de8da
Amauri <amauri.s.junior6@aluno.senai.br>
2024-11-23 11:40:36 -0300

    Open CV e comentários
    Adicionamos comentários para que os novos membros ou pessos leigas correlação a biblioteca da FTC possam ter maior entendiento do código. Além disso instalamos o Open CV que será utilizado para a detecção de objeto em específico para compreender a localização do robô.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/TeleOp.java
@@ -17,12 +17,14 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOperado Dox Cria", group = "OpMode")
 public class TeleOp extends OpMode {
-    DcMotorEx MET, MEF, MDT, MDF, LSi, LSii, braço, motorSla;
-    Servo yawC, garra;
+    DcMotorEx MET, MEF, MDT, MDF, LSi, LSii, braço, motorSla; //Define os motores no sistema
+    Servo yawC, garra; //Define o nome dos servos no sistema
     boolean yawG, raw;
-    ElapsedTime f = new ElapsedTime();
+    ElapsedTime f = new ElapsedTime(); //define contador de tempo no sistema
 
+    //Função de init
     public void init(){
+        //Definição dos motores e servos para o Drive Hub
         MET = hardwareMap.get(DcMotorEx.class, "MET");
         MDT = hardwareMap.get(DcMotorEx.class, "MDT");
         MEF = hardwareMap.get(DcMotorEx.class, "MEF");
@@ -34,12 +36,14 @@ public class TeleOp extends OpMode {
         braço = hardwareMap.get(DcMotorEx.class, "braço");
         motorSla = hardwareMap.get(DcMotorEx.class, "motorSla");
 
+        //Define a direção dos motores
         MET.setDirection(DcMotorSimple.Direction.REVERSE);
         MEF.setDirection(DcMotorSimple.Direction.REVERSE);
         LSi.setDirection(DcMotorSimple.Direction.REVERSE);
         braço.setDirection(DcMotorSimple.Direction.REVERSE);
         motorSla.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //Redefine os valores dos motores
         MDF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         MEF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         MET.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -47,6 +51,7 @@ public class TeleOp extends OpMode {
         LSi.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LSii.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        //Define o metodo a ser usado de contabilização dos motores
         MDF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         MEF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         MET.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -54,29 +59,32 @@ public class TeleOp extends OpMode {
         LSi.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         LSii.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        //define o meio de "freio" para os motores
         LSi.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         LSii.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         braço.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        //pré-definições antes de iniciar as funções
         yawC.setPosition(0);
         garra.setPosition(0.6);
         yawG = false;
         raw = false;
         f.reset();
         f.startTime();
-        double max, min;
-        telemetry.addLine("É O BOTÃO B DO CONTROLE 2, SE VCS ME PERGUNTAREM NO ZAP EU DEITO UM");
     }
+
+    //funções que vão se repetir, utilizadas para a partida em si e contêm os sistemas
     public void loop(){
-        movi();
-        linear();
-        arm();
+        movi(); //função da movimentação
+        linear(); //função do sistema linear
+        arm(); //função do braço
         if (f.seconds() >= 1) {
-            Crvos();
+            Crvos(); // função dos servos
         }
     }
 
     public void movi(){
+        //cálculos usados para obter os valores para as mecanum
         double axial   = gamepad1.left_trigger - gamepad1.right_trigger;
         double lateral = gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
@@ -101,7 +109,7 @@ public class TeleOp extends OpMode {
         telemetry.addData("MET", MET.getCurrentPosition());
         telemetry.addData("MDT", MDT.getCurrentPosition());
     }
-    public void MotorsPower(double p1, double p2, double p3,double p4){
+    public void MotorsPower(double p1, double p2, double p3,double p4) {
         MEF.setPower(p1);
         MDF.setPower(p2);
         MET.setPower(p3);
@@ -109,6 +117,7 @@ public class TeleOp extends OpMode {
     }
 
     public void Crvos(){
+        //Angulação da garra
         if (gamepad2.y && !yawG){
             yawC.setPosition(0.65);
             yawG = true;
@@ -120,6 +129,7 @@ public class TeleOp extends OpMode {
             f.reset();
         }
 
+        //Abrir/fechar a garra
         if (gamepad2.x && !raw){
             garra.setPosition(1);
             raw = false;
@@ -133,13 +143,12 @@ public class TeleOp extends OpMode {
         telemetry.addData("Garra está em:", garra.getPosition());
     }
 
-
-
-
     public void linear(){
+        //funções do linear
         LSi.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
         LSii.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
 
+        //motor novo implementado
         if (gamepad2.b){
             motorSla.setPower(1);
         }
@@ -149,6 +158,7 @@ public class TeleOp extends OpMode {
     }
 
     public void arm(){
+        //define a força do braço
         if (gamepad2.right_bumper){
             braço.setPower(0.8);
         }
